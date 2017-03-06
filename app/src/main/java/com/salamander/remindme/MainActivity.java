81e62949bedbd5631b57113eeb0031238f8ea99c
AlexNeiko <3ddvisio@gmail.com>
2017-03-06 17:12:27 +0200

    create activity

---
+++ app/src/main/java/com/salamander/remindme/MainActivity.java
@@ -0,0 +1,18 @@
+package com.salamander.remindme;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Alik on 06.03.2017.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
