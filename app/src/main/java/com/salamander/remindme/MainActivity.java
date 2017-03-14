b8db889cfceaa9dc5df32baf26b9e3fc558c00a6
AlexNeiko <3ddvisio@gmail.com>
2017-03-14 17:57:34 +0200

    navigation view

--- app/src/main/java/com/salamander/remindme/MainActivity.java
+++ app/src/main/java/com/salamander/remindme/MainActivity.java
@@ -1,23 +1,41 @@
 package com.salamander.remindme;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
+import com.salamander.remindme.adapter.TabsPagerFragmentAdapter;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int LAYOUT =R.layout.activity_main; //выносим в константу нужное активити.
 
     private Toolbar toolbar; //переменная для тулбара меню
+    private DrawerLayout drawerLayout;
+    private ViewPager viewPager; //переменная верхнего, горизонтальног меню
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setTheme(R.style.AppDelault); //в стилях создали свою тему и тут прописываем ее для активити
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
         
         initToolbar(); //инициализируем меню тулбара
+        initNavigationView(); //инцициализируем метод для пунктов меню navigationbar
+        initTabs();
+
+
+
     }
 
     //метод создания меню тулбара
@@ -32,5 +50,41 @@ public class MainActivity extends Activity {
         });
 
         toolbar.inflateMenu(R.menu.nemu);
+    }
+
+    private void initTabs() {
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        TabsPagerFragmentAdapter adapter = new TabsPagerFragmentAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout); //подключаемся к верхнему меню
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+
+    //метод инициализации пунктов меню navigationbar
+    private void initNavigationView() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout); //получаем наш слой с меню
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.view_navigation_open, R.string.view_navigation_close);
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                drawerLayout.closeDrawers();
+                switch (menuItem.getItemId()) {
+                    case R.id.actionNotificationItem:
+                        showNotificationTab();
+                }
+                return true;
+            }
+        });
+    }
+
+    private void showNotificationTab() {
+        viewPager.setCurrentItem(Constants.TAB_TWO);
     }
 }
---
+++ app/src/main/java/com/salamander/remindme/adapter/TabsPagerFragmentAdapter.java
@@ -0,0 +1,46 @@
+package com.salamander.remindme.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.salamander.remindme.fragment.ExampleFragment;
+
+public class TabsPagerFragmentAdapter extends FragmentPagerAdapter {
+
+    private String[] tabs; //переменная для пунктов меню
+
+    public TabsPagerFragmentAdapter(FragmentManager fm) {
+        super(fm);
+        tabs = new String[] { //перечисляем пункты меню
+                "Tab1",
+                "Напоминания",
+                "Tab2"
+
+        };
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabs[position];
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return ExampleFragment.getInstance();
+            case 1:
+                return ExampleFragment.getInstance();
+            case 2:
+                return ExampleFragment.getInstance();
+        }
+
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return tabs.length;
+    }
+}
